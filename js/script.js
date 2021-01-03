5029abb83de1cc3dbd190c0aabe306075c5a7185
samiultuhin754 <73142944+samiultuhin754@users.noreply.github.com>
2021-01-03 15:49:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+new WOW().init();
+
+/**
+     * Demo 1
+     */
+    var arc = function (t)
+    {
+        return {
+            x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+            y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+        };
+    };
+
+    $('#text1').curvedText({
+        curve:    arc,
+        domain:   [-0.2, 0.2],
+        viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
+    });
